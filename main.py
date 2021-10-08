9b8161e7babaa61d163760d371abba1cb2e6798c
Sos <sosantiart117@gmail.com>
2021-10-08 16:43:59 -0500

    make america great again
    This reverts commit 42e41dbd48ab1b5dbc217862357e712f71333f4d.

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
-class Sos:
-    def __init__(self):
-        self.edad = 23
-        self.travieso = True
-
-    def di_algo(self): 
-        print("Eres una puerca mi amor")
-
-    def como_estas(self):
-        if(self.travieso):
-            print("\nando_travieso")
-        else:
-            print("bien bueno")
-
 def main():
-    s = Sos()
-    s.di_algo()
-    s.como_estas()
-
-
+    print("MUAJAJAJJAJAJA VETE A LA VERGA ! \nTE CREAS XD")
+# este wey no sabe programar
 if __name__ == '__main__':
     main()
