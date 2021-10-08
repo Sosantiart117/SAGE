9b5dc880c6156fc630f24a355c9a7e50f6976365
Sos <sosantiart117@gmail.com>
2021-10-08 16:41:07 -0500

    Corregi tu chingadera

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-import Sos
+class Sos:
+    def __init__(self):
+        self.edad = 23
+        self.travieso = True
 
-s = Sos()
+    def di_algo(self): 
+        print("Eres una puerca mi amor")
 
-s.di_algo()
-s.como_estas()
+    def como_estas(self):
+        if(self.travieso):
+            print("\nando_travieso")
+        else:
+            print("bien bueno")
+
+def main():
+    s = Sos()
+    s.di_algo()
+    s.como_estas()
 
 
 if __name__ == '__main__':
