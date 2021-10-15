ca9296b6b95cdbb4824b6c1e5ab8c5521bb683ec
darklinklml <gpc9815@gmail.com>
2021-10-15 15:01:19 -0500

    Modificación Final Creación DB
    Se adjunta el archivo final .py

--- Build_DB.py
+++ Build_DB.py
@@ -5,15 +5,15 @@ con = sqlite3.connect("App_Data.db")
 try:
     # TABLA PROYECTOS
     con.execute("""
-        CREATE TABLE "proyectos" (
-            "Id_proyecto"	INTEGER,
-            "Titulo"	TEXT,
-            "Descripcion"	LONGTEXT,
-            "Estatus"	INTEGER DEFAULT 1,
-            "Ruta_Carpeta"	longtext,
-            "Fecha_Creacion"	INTEGER,
-            PRIMARY KEY("Id_proyecto" AUTOINCREMENT)
-        );
+            CREATE TABLE "proyectos" (
+                "Id_proyecto"	INTEGER,
+                "Titulo"	TEXT,
+                "Descripcion"	LONGTEXT,
+                "Estatus"	INTEGER DEFAULT 1,
+                "Ruta_Carpeta"	longtext,
+                "Fecha_Creacion"	INTEGER,
+                PRIMARY KEY("Id_proyecto" AUTOINCREMENT)
+            );
     """) 
 
     #TABLA CATEGORÍAS
@@ -33,12 +33,36 @@ try:
     
     #TABLA ETAPAS
     con.execute("""
-            
+                CREATE TABLE "etapas" (
+                    "Id_etapa"	INTEGER UNIQUE,
+                    "Titulo"	TEXT,
+                    "Categoria_Id"	INTEGER,
+                    "Ruta_Carpeta"	TEXT,
+                    "Fecha_Creacion"	INTEGER,
+                    FOREIGN KEY("Categoria_Id") REFERENCES "categorias"("Id_categorias"),
+                    PRIMARY KEY("Id_etapa" AUTOINCREMENT)
+                );    
     """)  
 
     #TABLA TASKS
     con.execute("""
-            
+                CREATE TABLE "tasks" (
+                    "Id_Tasks"	INTEGER,
+                    "Titulo"	TEXT DEFAULT 'Sin Titulo',
+                    "Descripcion"	LONGTEXT,
+                    "Estatus"	TEXT DEFAULT 'Registrada',
+                    "Proyecto_Id"	INTEGER,
+                    "Categoria_Id"	INTEGER,
+                    "Etapa_Id"	INTEGER,
+                    "Ruta_Carpeta"	TEXT,
+                    "Fecha_Creación"	INTEGER,
+                    "Fecha_Inicial"	INTEGER,
+                    "Fecha_Final"	INTEGER,
+                    PRIMARY KEY("Id_Tasks" AUTOINCREMENT),
+                    FOREIGN KEY("Categoria_Id") REFERENCES "categorias"("Id_categorias"),
+                    FOREIGN KEY("Proyecto_Id") REFERENCES "proyectos"("Id_proyecto"),
+                    FOREIGN KEY("Etapa_Id") REFERENCES "etapas"("Id_etapa")
+                );   
     """)  
 
     print("Se creó la tabla correctamente")
@@ -55,31 +79,6 @@ con.close()
 
 
 
-# CREATE TABLE "etapas" (
-# 	"Id_etapa"	INTEGER UNIQUE,
-# 	"Titulo"	TEXT,
-# 	"Categoria_Id"	INTEGER,
-# 	"Ruta_Carpeta"	TEXT,
-# 	"Fecha_Creacion"	INTEGER,
-# 	FOREIGN KEY("Categoria_Id") REFERENCES "categorias"("Id_categorias"),
-# 	PRIMARY KEY("Id_etapa" AUTOINCREMENT)
-# );
 
 
-# CREATE TABLE "tasks" (
-# 	"Id_Tasks"	INTEGER,
-# 	"Titulo"	TEXT DEFAULT 'Sin Titulo',
-# 	"Descripcion"	LONGTEXT,
-# 	"Estatus"	TEXT DEFAULT 'Registrada',
-# 	"Proyecto_Id"	INTEGER,
-# 	"Categoria_Id"	INTEGER,
-# 	"Etapa_Id"	INTEGER,
-# 	"Ruta_Carpeta"	TEXT,
-# 	"Fecha_Creación"	INTEGER,
-# 	"Fecha_Inicial"	INTEGER,
-# 	"Fecha_Final"	INTEGER,
-# 	PRIMARY KEY("Id_Tasks" AUTOINCREMENT),
-# 	FOREIGN KEY("Categoria_Id") REFERENCES "categorias"("Id_categorias"),
-# 	FOREIGN KEY("Proyecto_Id") REFERENCES "proyectos"("Id_proyecto"),
-# 	FOREIGN KEY("Etapa_Id") REFERENCES "etapas"("Id_etapa")
-# );
+
