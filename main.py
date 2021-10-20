264345991342cbc1c1f305f22d882ed1848f917d
Sos <sosantiart117@gmail.com>
2021-10-20 09:51:41 -0500

    added mainlayout and class SageZone to load Zones

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
+# Sage
 from modulos.Sage import Sage
-from modulos.tasks import Task as tarea
+# Kivy
+import kivy
+kivy.require('2.0.0')
+from kivy.app import App
+# from kivy.metrics import dp
+# from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty, BooleanProperty
+from kivy.uix.screenmanager import ScreenManager
 
-# def tareas():
-#     tarea.push(Nueva tarea)
+class SageApp(App):
+    """Clase de la app"""
+    def build(self):
+        return MainLayout()
 
+class MainLayout(FloatLayout):
+    pass
+
+
+class MainBar(BoxLayout):
+    title = StringProperty("Tareas")
+
+    def set_title(self,title):
+        self.title = title
+
+class SageZone(ScreenManager):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
 
 def main():
-    """ Metodo principal del menu principal"""
-
     Sage.init()
+    SageApp().run()
 
 if __name__ == '__main__':
     main()
